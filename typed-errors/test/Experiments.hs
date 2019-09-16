8c88e1ef2322e50ab26cf5177fcead9e4e07937f
Rohit Ramesh <rkr@berkeley.edu>
2019-09-16 16:37:18 -0700

    Added some errors from EDG that were causing errors.

--- typed-errors/test/Experiments.hs
+++ typed-errors/test/Experiments.hs
@@ -29,8 +29,14 @@ class (Ord a, Monoid b) => CrazyErr a b c e where
   err2 :: forall d. (Ord d) => a -> b -> c d -> e
   err3 :: forall m. (InternalErr e) => (c m -> Maybe m) -> c b -> c a -> e -> e
 
-makeErrClassHelpers defaultTypedErrorRules ''CrazyErr
-makeErrClassHelpers defaultTypedErrorRules ''InternalErr
+class ConversionErr i a e where
+  invalidMsgOp     :: (Show msgOp ) => Text -> msgOp -> e
+  invalidTermOp    :: (Show termOp) => Text -> termOp -> e
+  invalidParamInit :: (Show pInit ) => Text -> pInit -> e
+
+-- makeErrClassHelpers defaultTypedErrorRules ''CrazyErr
+makeErrClassHelpers defaultTypedErrorRules ''ConversionErr
+-- makeErrClassHelpers defaultTypedErrorRules ''InternalErr
 
 main :: IO ()
 main = print "It compiled!"
